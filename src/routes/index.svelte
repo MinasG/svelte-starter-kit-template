91110bae064fc79c29149bd34bc8de0120e14905
Minas Giannopoulos <hello@minasg.com>
2022-06-06 17:48:30 +0100

    Sets up head for the index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,10 @@
+<svelte:head>
+  <html lang="en" />
+  <meta name="robots" content="noindex nofollow" />
+  <meta name="description" content="A SvelteKit template with TS and tailwindcss" />
+  <title>Hello, world!</title>
+</svelte:head>
+
 <section class="h-screen flex flex-col items-center justify-center">
   <h1 class="text-xl font-bold mb-2">Hello, world!</h1>
   <h2>Welcome to SvelteKit with TS and tailwindcss.</h2>
